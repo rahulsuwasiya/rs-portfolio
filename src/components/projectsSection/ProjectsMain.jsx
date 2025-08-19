8f7588971e35b5d290967c6c68c45a89f984b4dd
rahulsuwasiya <rahulsuwasiya2004@gmail.com>
2025-08-19 17:33:04 +0530

    project link added

--- src/components/projectsSection/ProjectsMain.jsx
+++ src/components/projectsSection/ProjectsMain.jsx
@@ -55,6 +55,7 @@ const ProjectsMain = () => {
               year={project.year}
               align={project.align}
               image={project.image}
+              link={project.link}
             />
           );
         })}
